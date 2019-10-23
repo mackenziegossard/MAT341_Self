c54d2089482d7ea3605516859d02cbe345e1164e
mackenziegossard <gossarm1@tcnj.edu>
2019-10-23 10:28:52 -0400

    Second commit of HW4

--- HWs/HW4/successive_Parabolic_Interpolation.m
+++ HWs/HW4/successive_Parabolic_Interpolation.m
@@ -1,21 +1,41 @@
+%
+% Script that runs the successive parabolic interpolation algorithm to 
+% find the minimum of a certain function
+%
+% Input: tol - error tolerance
+%
+
 function N = successive_Parabolic_Interpolation(tol)
 
+% initilizes three points along the function
 x1 = 0;
 x2 = .6;
 x3 = 1.2;
+
+% initializes the number of iterations, N, at 0
 N = 0;
 
+% run the algorithm until x3-x1 is less than the tolerance
 while abs(x3 - x1) > tol
+    % finds a parabola through the three points using a linear system
     A = [x1^2, x1, 1, f(x1); x2^2, x2, 1, f(x2); x3^2, x3, 1, f(x3)];
     B = rref(A);
+    % finds the minimum value of the parabola 
     xMin = -B(2,4) / (2*B(1,4));
+    % reset x1, x2, x3
     x3 = x2;
     x2 = x1;
     x1 = xMin;
+    % increments the iteration counter
     N = N + 1;
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Function that you are trying to minimize
+% Input: x value
+%
+
 function val = f(x)
 val = .5 - x*exp(-(x^2));
 
--- HWs/HW4/vary_Error_Tolerances_To_Compare.m
+++ HWs/HW4/vary_Error_Tolerances_To_Compare.m
@@ -1,14 +1,26 @@
+%
+% Functions that calculates the number of iterations, N, need to achieve
+% varying error tolerances for two different algorithms, golden search and 
+% succesive parabolic interpolation, and graphs the results
+%
+
 function vary_Error_Tolerances_To_Compare()
 
+% error tolerance to test
 errTolVec = [1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12];
 
+% loops over all the tolerances
 for i = 1:length(errTolVec)
+    % runs the golden search algorithm for a specific tolerance
     golden(i) = golden_Search(errTolVec(i));
+    % runs the successive parabolic interpolation algorithm for a specific
+    % tolerance
     parabolic(i) = successive_Parabolic_Interpolation(errTolVec(i));
 end
 
 lw = 5;
 
+% plot using logarithmic axis in the horizontal direction 
 figure(1)
 semilogx(errTolVec, golden, 'LineWidth',lw)
 hold on
@@ -18,6 +30,7 @@ xlabel('tol');
 ylabel('N');
 legend('Golden Search','Succ. Para. Interp.')
 
+% plot using logarithmic axis in both direction
 figure(2)
 loglog(errTolVec, golden, 'LineWidth',lw)
 hold on
