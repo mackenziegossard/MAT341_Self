9a7539eb6f03bfda2338ea46880cc4542d3f1dc7
mackenziegossard <gossarm1@tcnj.edu>
2019-11-15 11:51:52 -0500

    commit of gradient decent

--- Class/Optimization/gradient_Descent.m
+++ Class/Optimization/gradient_Descent.m
@@ -12,3 +12,27 @@
 % gammaN = |(Xm-Xn)^t * (gradF(Xm)-gradF(Xn)| / ||gradF(Xm)-gradF(Xn)||2
 
 function val = gradient_Desecent()
+
+err = 1;
+tol = 1e-8;
+N = 0;
+
+gamma_n = 0.9;
+Xn = [1; 1.5];
+
+while err > tol
+  
+    Xm = Xn - gamma_n * gradF(Xn);
+    err = sqrt((Xm - Xn).' * (Xm - Xn));
+    gamma_n = abs((Xm-Xn).'*(gradF(Xm)-gradF(Xn))) / ((gradF(Xm)-gradF(Xn)).'*(gradF(Xm)-gradF(Xn)))
+    Xn = Xm;
+    N = N+1;
+end
+Xn
+N
+
+function val = gradF(x)
+val = [-cos(x(1)); sin(x(2))];
+
+% when gamma = 0.5, n = 28
+% when gamma = 0.9, n = 10
